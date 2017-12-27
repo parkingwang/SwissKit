ac09d391db45a4004f3a84c912b5ae44a77195b5
yoojia.chen <yoojiachen@gmail.com>
2017-12-27 12:30:48 +0800

    Update

--- src/main/java/com/parkingwang/lang/kit/MapReader.java
+++ src/main/java/com/parkingwang/lang/kit/MapReader.java
@@ -1,10 +1,6 @@
 package com.parkingwang.lang.kit;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Queue;
+import java.util.*;
 import java.util.concurrent.ArrayBlockingQueue;
 
 /**
